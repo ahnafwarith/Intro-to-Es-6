5d0f109fb6c85954bbac9f569d0b71367b7b3816
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-02-19 20:48:37 +0600

    added new

--- template-string.js
+++ template-string.js
@@ -1,2 +1,10 @@
 const priya = 'Asif Priya';
-const meye = "Meye"
+const meye = "Meye";
+const kobu = `kire?`
+// `` is bacically used to write multiline comments. And work with variables in a less complex way.
+const friends = ['abul', 'babul', 'kabul', 'sabul'];
+const count = 5;
+const old1 = '<h3 class="friend-name">Friend-5</h3>';
+const old2 = '<h3 class="friend-name">Friend-' + count + '</h3>';
+const new2 = `<h3 class="friend-name">Friend-${count}</h3>`
+const new1 = `<h3 class="friend-name">Friend-${friends.length}</h3>`
